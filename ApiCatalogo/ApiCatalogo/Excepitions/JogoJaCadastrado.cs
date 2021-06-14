9a48fc90f7ace73adfac1a2c6708e6ff750090b2
João Barbosa <84736453+jota-duck@users.noreply.github.com>
2021-06-14 19:41:05 -0300

    Add files via upload

---
+++ ApiCatalogo/ApiCatalogo/Excepitions/JogoJaCadastrado.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ApiCatalogo.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este já jogo está cadastrado")
+        { }
+    }
+}
